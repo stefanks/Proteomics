c793ffe1c28414f213782f4b4d95feafe333f3d4
stefanks <solntsev@gmail.com>
2016-05-24 11:09:27 -0500

    Init

--- Proteomics/Properties/AssemblyInfo.cs
+++ Proteomics/Properties/AssemblyInfo.cs
@@ -6,21 +6,20 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Proteomics")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for basic Proteomics objects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Stefan Solntsev")]
 [assembly: AssemblyProduct("Proteomics")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright  2016 Stefan Solntsev")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ab6ba94-b70c-4570-9ea6-165746d5b8e8")]
+[assembly: Guid("da8e4387-675c-42e7-9444-770548a5108e")]
 
 // Version information for an assembly consists of the following four values:
 //
