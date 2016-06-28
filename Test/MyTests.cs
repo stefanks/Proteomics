003ae2c5397246281c2ac49f22070381098d194e
stefan <solntsev@wisc.edu>
2016-06-28 18:18:28 -0500

    more coverage

--- Test/MyTests.cs
+++ Test/MyTests.cs
@@ -14,9 +14,11 @@ namespace Test
             Modification mod1 = new Modification(10, "mass 10 modification");
             Modification mod2 = new Modification(100, "mass 100 modification");
             Modification mod3 = new Modification(1000, "mass 1000 modification");
-            ModificationCollection a = new ModificationCollection(mod1, mod2, mod3);
-            ModificationCollection b = new ModificationCollection(mod1, mod3, mod2);
+            ModificationCollection a = new ModificationCollection(mod1, mod2, mod3, mod1);
+            ModificationCollection b = new ModificationCollection(mod1, mod3, mod1, mod2);
             Assert.IsTrue(a.Equals(b));
+            ModificationCollection c = new ModificationCollection(mod1);
+            Assert.IsFalse(c.Equals(b));
         }
     }
 }
